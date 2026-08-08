82b501e1a18eb5501acebed347a73cb77d6f7944
Matt Fehrenbach <github.4gsly@passmail.com>
2026-08-08 18:43:31 +0200

    feat(music): update music page layout and enhance embedded media components

--- src/components/embedded-media.tsx
+++ src/components/embedded-media.tsx
@@ -9,8 +9,8 @@ export function BandcampEmbed({
 }) {
   return (
     <iframe
-      className="w-full shrink-0 h-11"
-      src={`https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=small/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/`}
+      className="w-full aspect-square grayscale"
+      src={`https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/`}
       allow="encrypted-media; fullscreen; autoplay; idle-detection; speaker-selection; web-share;"
       title={title}
       seamless
@@ -20,13 +20,24 @@ export function BandcampEmbed({
   );
 }
 
-export function MixcloudEmbed({ feed, title }: { feed: string; title: string }) {
+export function MixcloudEmbed({
+  feed,
+  title,
+  artworkSrc,
+}: {
+  feed: string;
+  title: string;
+  artworkSrc: string;
+}) {
   return (
-    <iframe
-      className="w-full shrink-0 h-30"
-      src={`https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&hide_artwork=1&feed=${feed}`}
-      title={title}
-      allow="encrypted-media; fullscreen; autoplay; idle-detection; speaker-selection; web-share;"
-    />
+    <div className="group relative w-full aspect-square grayscale">
+      <img src={artworkSrc} alt="" className="absolute w-full aspect-square object-cover" />
+      <iframe
+        className="absolute w-full aspect-square opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity"
+        src={`https://player-widget.mixcloud.com/widget/iframe/?mini=false&hide_cover=false&hide_artwork=true&feed=${feed}`}
+        title={title}
+        allow="encrypted-media; fullscreen; autoplay; idle-detection; speaker-selection; web-share;"
+      />
+    </div>
   );
 }
