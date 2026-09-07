2ea333c13be5197a23c2db87851380fa4662c9d8
Matt Fehrenbach <github.4gsly@passmail.com>
2026-09-07 20:40:45 +0200

    feat(seo): add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: "https://m4t.tf",
   integrations: [
     react(),
+    sitemap(),
     mdx({
       processor: unified({ remarkPlugins: [remarkReadingTime] }),
     }),
